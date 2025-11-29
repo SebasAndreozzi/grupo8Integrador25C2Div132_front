57237e870b657e9271f6ef8d5bb57c8003834e8f
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2025-11-29 12:54:18 -0300

    Agrego contador de productos del carrito

--- js/index.js
+++ js/index.js
@@ -1,142 +1,152 @@
-        let contenedorProductos = document.getElementById("contenedor-productos");
-        let contenedorCarrito = document.getElementById("contenedor-carrito")
-        let carrito = [];
-        
-        async function obtenerProductos(){
-            try {
-                // Hago el fetch a la url personalizada
-                let response = await fetch(`http://localhost:3000/api/productos/cliente`);
+let contenedorProductos = document.getElementById("contenedor-productos");
+let contenedorCarrito = document.getElementById("contenedor-carrito")
+let carrito = [];
 
-                // Proceso los datos que me devuelve el servidor
-                let datos = await response.json();
+async function obtenerProductos(){
+    try {
+        // Hago el fetch a la url personalizada
+        let response = await fetch(`http://localhost:3000/api/productos/cliente`);
 
-                if (!response.ok) {
-                    mostrarError(datos.message || "No se pudo obtener el producto");
-                    return;
-                }
-                // Extraigo el producto que devuelve payload
-                let productos = datos.payload; // Apuntamos a la respuesta, vamos a payload que trae el array con el objeto y extraemos el primer y unico elemento
+        // Proceso los datos que me devuelve el servidor
+        let datos = await response.json();
 
-                // Le pasamos el producto a una funcion que lo renderice en la pantalla
-                mostrarProductos(productos); 
-
-            } catch (error) {
-                console.error("Error: ", error);
-            }
-
+        if (!response.ok) {
+            mostrarError(datos.message || "No se pudo obtener el producto");
+            return;
         }
-        
-        function mostrarProductos(array)
-        {
-            let htmlProducto ="";
+        // Extraigo el producto que devuelve payload
+        let productos = datos.payload; // Apuntamos a la respuesta, vamos a payload que trae el array con el objeto y extraemos el primer y unico elemento
 
-            array.forEach(producto=> {
-                    htmlProducto += `
-                    <div class= "card-producto">
-                        <img src="${producto.img}" alt="${producto.nombre}">
-                        <h3>${producto.nombre}</h3>
-                        <p>$${producto.precio}</p>
-                        <button class="button" data-producto='${JSON.stringify(producto)}' onclick="agregarACarrito(this.dataset.producto)">Agregar al carrito</button>
-                    </div>
-                `;
-        
-            });
-            contenedorProductos.innerHTML = htmlProducto;
+        // Le pasamos el producto a una funcion que lo renderice en la pantalla
+        mostrarProductos(productos);
+        iniciarFiltros(productos);
 
-            let sellado = document.getElementById("filtro-sellado");
-            sellado.addEventListener("click", event =>{
-                filtrarPorTipo(array, sellado.textContent);
-            })
+    } catch (error) {
+        console.error("Error: ", error);
+    }
 
-            let accesorio = document.getElementById("filtro-accesorio");
-            accesorio.addEventListener("click", event =>{
-                filtrarPorTipo(array, accesorio.textContent);
-            })
+}
 
-            let carrito = document.getElementById("carrito");
-            carrito.addEventListener("click", (event) =>{
-                mostrarCarrito(array);
-            })
-        
-        }
+function mostrarProductos(array)
+{
+    let htmlProducto ="";
 
-        /*====================
-        Funcines de carrito
-        =====================*/
-        function agregarACarrito(producto){ 
-            carrito.push(producto);
-            if(contenedorCarrito.innerHTML.trim() != ''){
-                mostrarCarrito();
-            }
-        }
+    array.forEach(producto=> {
+            htmlProducto += `
+            <div class= "card-producto">
+                <img src="${producto.img}" alt="${producto.nombre}">
+                <h3>${producto.nombre}</h3>
+                <p>$${producto.precio}</p>
+                <button class="button" data-producto='${JSON.stringify(producto)}' onclick="agregarACarrito(this.dataset.producto)">Agregar al carrito</button>
+            </div>
+        `;
 
-        function mostrarCarrito(){
+    });
+    contenedorProductos.innerHTML = htmlProducto;
+}
 
-            let htmlCarrito ="";
+/*===================
+Funcines de filtro
+====================*/
+function iniciarFiltros(array){
+    let sellado = document.getElementById("filtro-sellado");
+    sellado.addEventListener("click", () =>{
+        filtrarPorTipo(array, sellado.textContent);
+    })
 
-            carrito.forEach((producto, i)=> {
-                let prod = JSON.parse(producto);
+    let accesorio = document.getElementById("filtro-accesorio");
+    accesorio.addEventListener("click", () =>{
+        filtrarPorTipo(array, accesorio.textContent);
+    })
+}
 
-                htmlCarrito += `
-                    <div class= "card-producto">
-                        <img src="${prod.img}" alt="${prod.nombre}">
-                        <h3>${prod.nombre}</h3>
-                        <p>$${prod.precio}</p>
-                        <button class="button" onclick="eliminarElemento(${i})">Eliminar</button>
-                    </div>
-                `; 
-            });
+function filtrarPorTipo(array, tipo){
+    let htmlProducto ="";
 
-            htmlCarrito += `<button class=button>Finalizar compra</button>`
-
-            if(carrito.length > 0){
-                contenedorCarrito.innerHTML = htmlCarrito;
-            }
-
-        }
-
-        function eliminarElemento(indice){
-            carrito.splice(indice, 1);
-
-            if(carrito.length === 0){
-                vaciarCarrito();
-
-            }else{
-                mostrarCarrito();
-            }
-        }
-
-        function vaciarCarrito(){
-            carrito = [];
-            contenedorCarrito.innerHTML = "";
-        }
-
-        /*===================
-        Funcines de filtro
-        ====================*/
-        function filtrarPorTipo(array, tipo){
-            let htmlProducto ="";
-
-            array.forEach(producto=> {
-                if(producto.tipo === tipo){
-                    htmlProducto += `
-                        <div class= "card-producto">
-                            <img src="${producto.img}" alt="${producto.nombre}">
-                            <h3>${producto.nombre}</h3>
-                            <p>$${producto.precio}</p>
-                            <button class="button" onclick="agregarACarrito(${array})">Agregar al carrito</button>
-                        </div>
-                    `;
-                }
-
-
-            });
-            contenedorProductos.innerHTML = htmlProducto;
+    array.forEach(producto=> {
+        if(producto.tipo === tipo){
+            htmlProducto += `
+                <div class= "card-producto">
+                    <img src="${producto.img}" alt="${producto.nombre}">
+                    <h3>${producto.nombre}</h3>
+                    <p>$${producto.precio}</p>
+                    <button class="button" onclick="agregarACarrito(${array})">Agregar al carrito</button>
+                </div>
+            `;
         }
 
 
-        function init() {
-            obtenerProductos();
-        }
+    });
+    contenedorProductos.innerHTML = htmlProducto;
+}
 
-        init();
+/*====================
+Funcines de carrito
+=====================*/
+
+let imgCarrito = document.getElementById("img-carrito");
+imgCarrito.addEventListener("click", () =>{
+    mostrarCarrito();
+})
+
+function mostrarCarrito(){
+
+    let htmlCarrito ="";
+
+    carrito.forEach((producto, i)=> {
+        let prod = JSON.parse(producto);
+
+        htmlCarrito += `
+            <div class= "card-producto">
+                <img src="${prod.img}" alt="${prod.nombre}">
+                <h3>${prod.nombre}</h3>
+                <p>$${prod.precio}</p>
+                <button class="button" onclick="eliminarElemento(${i})">Eliminar</button>
+            </div>
+        `; 
+    });
+
+    htmlCarrito += `<button class=button>Finalizar compra</button>`
+
+    if(carrito.length > 0){
+        contenedorCarrito.innerHTML = htmlCarrito;
+    }
+
+}
+
+function actualizarCantidadCarrito(){
+    let contadorCarrito = document.getElementById("contador-carrito");
+    
+    contadorCarrito.innerHTML = `${carrito.length}`;
+}
+
+function agregarACarrito(producto){ 
+    carrito.push(producto);
+    actualizarCantidadCarrito();
+    if(contenedorCarrito.innerHTML.trim() != ''){
+        mostrarCarrito();
+    }
+}
+
+function eliminarElemento(indice){
+    carrito.splice(indice, 1);
+    actualizarCantidadCarrito();
+
+    if(carrito.length === 0){
+        vaciarCarrito();
+
+    }else{
+        mostrarCarrito();
+    }
+}
+
+function vaciarCarrito(){
+    carrito = [];
+    contenedorCarrito.innerHTML = "";
+}
+
+function init() {
+    obtenerProductos();
+}
+
+init();
